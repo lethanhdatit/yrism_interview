d0fc8b0ec11a2ab54113cd9b207931cbfa18985b
Le Thanh Dat <thanhdat.it.mmo@yrism.com>
2024-06-23 01:14:31 +0700

    release

--- Program.cs
+++ Program.cs
@@ -47,12 +47,8 @@ using (var scope = app.Services.CreateScope())
     DbInitializer.Initialize(context);
 }
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
